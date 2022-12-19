107ef30a22f6b3bbe952a2af83395aaeaf422fc3
Ismael Moreira <imcalazans@gmail.com>
2022-12-19 18:33:28 -0300

    Backend project created

---
+++ backend/src/main/java/com/ismaelMoreira/costumer/CostumerApplication.java
@@ -0,0 +1,13 @@
+package com.ismaelMoreira.costumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumerApplication.class, args);
+	}
+
+}
